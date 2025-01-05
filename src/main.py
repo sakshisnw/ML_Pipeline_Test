ab69c255ddec21e4d69ad684fd2dc0b88db80c51
sakshi <sakshi28112@gmail.com>
2025-01-05 23:25:56 +0530

    Day 2: Data cleaning and validation implemented

--- src/main.py
+++ src/main.py
@@ -1,17 +1,28 @@
-from src.pipeline import parse, extract
+from src.pipeline import parse, extract, dataset, clean_data, preprocess, TTS, t_model,e_model
 
 def main():
     path = "algoparams_from_ui.json.json"  
-    
     config = parse(path)
+    details = extract(config) 
     
-    details = extract(config)  
+    data = "data/iris.csv"
+    df = dataset(data)
+    df = clean_data(df)
+    features, target = preprocess(df, details["target"])
     
-    print("Target Variable:", details["target"])
-    print("Prediction Type:", details["prediction_type"])
-    print("Feature Handling Instructions:", details["feature_handling"])
-    print("Selected Algorithms:", details["sel_algorithm"]) 
-
+    X_train, X_test, y_train, y_test = TTS(features, target)
+    model = t_model(details["sel_algorithm"], X_train, y_train)
+    result = e_model(model, X_test, y_test)
+    
+    print("Model Evaluation Results:", result)
+    
+    #print("Target Variable:", details["target"])
+    #print("Prediction Type:", details["prediction_type"])
+    #print("Feature Handling Instructions:", details["feature_handling"])
+    #print("Selected Algorithms:", details["sel_algorithm"])
+     
+    #print("Sample Dataset of iris\n",df.head(5))
+    #print("Missing values in iris dataset",df.isnull().sum().sum())
     
 if __name__ == "__main__":
     main()
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,4 +1,10 @@
 import json
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+
 
 def parse(path):
     with open(path, 'r') as f:
@@ -21,3 +27,58 @@ def extract(config):
         "feature_handling": feature_handling,
         "sel_algorithm": sel_algorithm,
     }
+    
+def dataset(data):
+    return pd.read_csv(data)
+
+def clean_data(df):
+    if df.isnull().sum().sum() > 0:
+        df = df.dropna()    
+    return df
+
+def preprocess(df, target):
+    features = df.drop(columns=[target])
+    t_val = df[target]
+    
+    if t_val.dtypes == 'object':
+        encoder = LabelEncoder()
+        t_val = encoder.fit_transform(t_val)
+        
+    cat_cols = features.select_dtypes(include=['object']).columns
+    num_cols = features.select_dtypes(exclude=['object']).columns
+    
+    if len(cat_cols) > 0:
+        features[cat_cols] = features[cat_cols].apply(LabelEncoder().fit_transform)
+    
+    scaler = StandardScaler()
+    features[num_cols] = scaler.fit_transform(features[num_cols])
+     
+    return features, t_val
+
+
+def TTS(features, target):
+    return train_test_split(features, target, random_state=40, test_size=0.2)
+
+def t_model(m_config, X_train, y_train):
+    trained = {}
+    for m_name, params in m_config.items():
+        if m_name == "RandomForestRegressor":
+            valid_params = {k: v for k, v in params.items() if k in RandomForestRegressor().get_params()}
+            model = RandomForestRegressor(**valid_params)
+        elif m_name == "LinearRegression":
+            model = LinearRegression()
+        else:
+            continue
+        model.fit(X_train, y_train)
+        t_model[m_name] = model
+    return trained
+
+def e_model(models, X_test, y_test):
+    result = {}
+    for m_name, model in models.items():
+        pred = model.predict(X_test)
+        result[m_name] = pred
+    return result
+
+def e_model():
+    return
